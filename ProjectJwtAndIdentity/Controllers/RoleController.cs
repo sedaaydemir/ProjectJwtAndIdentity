6940bb1e2440386daf8249f0b74853108cf934c4
Seda Aydemir <seda.aydemir7@gmail.com>
2025-02-27 21:55:04 +0300

    ***

--- ProjectJwtAndIdentity/Controllers/RoleController.cs
+++ ProjectJwtAndIdentity/Controllers/RoleController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using ProjectJwtAndIdentity.Entities;
 using ProjectJwtAndIdentity.Models;
 
@@ -28,7 +29,68 @@ namespace ProjectJwtAndIdentity.Controllers
         [HttpPost]
         public async Task<IActionResult> CreateRole(CreateRoleViewModel model)
         {
-            return View();
+            AppRole appRole = new AppRole()
+            {
+                Name = model.RoleName
+
+            };
+            await _roleManager.CreateAsync(appRole);
+            return RedirectToAction("RoleList");
         }
+
+        public async Task<IActionResult> DeleteRole(int id)
+        {
+            var value = await _roleManager.Roles.FirstOrDefaultAsync(x => x.Id == id);
+            await _roleManager.DeleteAsync(value);
+            return RedirectToAction("RoleList");
+        }
+
+
+        public IActionResult UserList()
+        {
+            var values = _userManager.Users.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AssignRole(int id)
+        {
+            var user = await _userManager.Users.FirstOrDefaultAsync(x => x.Id == id);
+            TempData["x"] = user.Id;
+            var roles = _roleManager.Roles.ToList();
+            var userRoles = await _userManager.GetRolesAsync(user);
+
+            List<RoleAssignViewModel> roleAssignViewModels = new List<RoleAssignViewModel>();
+            foreach (var item in roles)
+            {
+                RoleAssignViewModel model = new RoleAssignViewModel();
+                model.RoleId = item.Id;
+                model.RoleName = item.Name;
+                model.RoleExist = userRoles.Contains(item.Name);
+                roleAssignViewModels.Add(model);
+            }
+            return View(roleAssignViewModels);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> AssignRole(List<RoleAssignViewModel> model)
+        {
+            var userid = (int)TempData["x"];
+            var user = await _userManager.Users.FirstOrDefaultAsync(x => x.Id == userid);
+            foreach (var item in model)
+            {
+                if (item.RoleExist)
+                {
+                    await _userManager.AddToRoleAsync(user, item.RoleName);
+                }
+                else
+                {
+                    await _userManager.RemoveFromRoleAsync(user, item.RoleName);
+                }
+            }
+            return RedirectToAction("UserList");
+        }
+
     }
 }
---
+++ ProjectJwtAndIdentity/Models/RoleAssignViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjectJwtAndIdentity.Models
+{
+    public class RoleAssignViewModel
+    {
+        public int RoleId { get; set; } 
+        public string RoleName { get; set; }
+
+        public bool RoleExist { get; set; }
+    }
+}
